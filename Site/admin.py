fa010070fc78616386acd8ae5d0db7136f57ede8
pablolx <pablo.pinheiro@gmail.com>
2025-04-15 07:56:47 -0300

    Meu Arquivos do MonitorBolsa

--- Site/admin.py
+++ Site/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ReportBolsa
 
 # Register your models here.
+
+@admin.register(ReportBolsa)
+class ReportBolsa(admin.ModelAdmin):
+    list_display = ('autor', 'titulo', 'texto', 'data_criacao', 'data_publicacao')
--- Site/models.py
+++ Site/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+
+class ReportBolsa(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(default=timezone.now)
+    data_publicacao = models.DateTimeField(blank=True, null=True)
+
+    def publicacao(self):
+        self.data_publicacao = timezone.now
+        self.save()
+
+    def __str__(self):
+        return self.titulo
